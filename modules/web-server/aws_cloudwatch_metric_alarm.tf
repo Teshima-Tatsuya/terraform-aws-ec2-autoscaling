be504a7f44133529895cf3345a366de67d357eb8
Teshima-Tatsuya <terima666tatsuya@gmail.com>
2023-10-10 16:05:53 +0900

    datapoint

--- modules/web-server/aws_cloudwatch_metric_alarm.tf
+++ modules/web-server/aws_cloudwatch_metric_alarm.tf
@@ -2,6 +2,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu" {
   alarm_name          = "CPUUtilization_websrv"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 2
+  datapoints_to_alarm = 1
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
   period              = 60
