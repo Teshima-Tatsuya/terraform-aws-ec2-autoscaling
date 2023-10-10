71f85ad3cb19dfac9be96dca25422079fe0d5b61
Teshima-Tatsuya <terima666tatsuya@gmail.com>
2023-10-10 17:07:39 +0900

    fix userdata

--- modules/web-server/userdata.sh
+++ modules/web-server/userdata.sh
@@ -11,4 +11,5 @@ yum install amazon-cloudwatch-agent -y
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-cli \
   -a fetch-config \
   -m ec2 \
+  -s \
   -c ssm:cloudwatch_agent_config
