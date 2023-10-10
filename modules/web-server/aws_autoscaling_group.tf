300422781c0cc57589ebed49da290f1da5b9bbfb
Teshima-Tatsuya <terima666tatsuya@gmail.com>
2023-10-10 11:14:30 +0900

    healthcheck grace period changed

--- modules/web-server/aws_autoscaling_group.tf
+++ modules/web-server/aws_autoscaling_group.tf
@@ -2,7 +2,7 @@ resource "aws_autoscaling_group" "web" {
   name                      = "teshima-asg"
   max_size                  = 1
   min_size                  = 1
-  health_check_grace_period = 60
+  health_check_grace_period = 180
   health_check_type         = "ELB"
   desired_capacity          = 1
   force_delete              = true
