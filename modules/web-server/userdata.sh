b0ee5ae364a93f7a029d3f82417ff876208eae42
Teshima-Tatsuya <terima666tatsuya@gmail.com>
2023-10-11 14:17:00 +0900

    fix namespace

--- modules/web-server/userdata.sh
+++ modules/web-server/userdata.sh
@@ -8,7 +8,7 @@ touch /var/www/html/index.html
 
 yum install amazon-cloudwatch-agent -y
 
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-cli \
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
   -a fetch-config \
   -m ec2 \
   -s \
