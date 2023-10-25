6953ba59b143ea1c072896ef420b90bdba236935
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-10-25 18:00:16 +0900

    remove prevent destroy

--- modules/domain/aws_acm_certificate.tf
+++ modules/domain/aws_acm_certificate.tf
@@ -1,8 +1,4 @@
 resource "aws_acm_certificate" "dev" {
   domain_name = "*.tessy.dev"
   validation_method = "DNS"
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
--- modules/domain/aws_route53_zone.tf
+++ modules/domain/aws_route53_zone.tf
@@ -1,7 +1,3 @@
 resource "aws_route53_zone" "dev" {
     name = "aws.tessy.dev"
-
-    lifecycle {
-      prevent_destroy = true
-    }
 }
